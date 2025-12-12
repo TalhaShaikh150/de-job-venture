9354ef6a78e25da146347fdeee1cf1f62a072140
TalhaShaikh150 <tssmart233@gmail.com>
2025-12-12 22:31:02 +0500

    Fix deployment: corrected Pages/JobListing import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import Home from "./Pages/Home";
 import JobListing from "./Pages/JobListing";
 
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Auth from "./Pages/Auth";
 import Aplication from "./Pages/Aplication";
 function App() {
   return (
     <>
-  
-
-  <Routes>
-      <Route path="/" element={<Home/>} />
-    <Route path="/listing" element={<JobListing/>}></Route>
-      <Route path="/auth" element={<Auth/>} />
-      <Route path="/application" element={<Aplication/>} />
-  </Routes>
-      {/* <JobListing/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listing" element={<JobListing />}/>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/application" element={<Aplication />} />
+      </Routes>
     </>
   );
 }
