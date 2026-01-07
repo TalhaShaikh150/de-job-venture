b292d4f649c643f942b4b9ab39acce23ff740267
itssadiq <sadiqalishaikh009@gmail.com>
2026-01-08 00:55:37 +0500

    Created about us page UI and connected with routes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import DejobSearch from "./Pages/JobListings";
+import AboutUs from "./Pages/AboutUs";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listing" element={<DejobSearch />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </>
   );
